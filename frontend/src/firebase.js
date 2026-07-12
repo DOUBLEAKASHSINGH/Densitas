1f6bfd4efa41009bea24cede00ba9220124f73a4
DOUBLEAKASHSINGH <akash24110@iiitd.ac.in>
2026-07-12 23:02:55 +0530

    Rename densitas to optiflow in firebase config

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -6,9 +6,9 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDA4aFqTgRuYems6O-G0AHJcJGrTEwDbEE",
-  authDomain: "densitas.firebaseapp.com",
-  projectId: "densitas",
-  storageBucket: "densitas.firebasestorage.app",
+  authDomain: "optiflow.firebaseapp.com",
+  projectId: "optiflow",
+  storageBucket: "optiflow.firebasestorage.app",
   messagingSenderId: "283296237927",
   appId: "1:283296237927:web:ae599760d8d8aa4a9f07c2",
   measurementId: "G-4L5DPZWBN0"
